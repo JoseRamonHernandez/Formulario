4b8daed24f4a5f2c5b7463474c290bb8ab93a5a3
JoseRamonHernandez <67391682+JoseRamonHernandez@users.noreply.github.com / joseramonhernandezmunoz20@gmail.com>
2020-11-12 16:45:06 -0600

    video 191
    Relacion-Tarea-Validaciones

--- src/app/pages/template/template.component.ts
+++ src/app/pages/template/template.component.ts
@@ -9,7 +9,9 @@ import { NgForm } from '@angular/forms';
 export class TemplateComponent implements OnInit {
 
 usuario = {
-  nombre: ''
+  nombre: 'ramon',
+  apellido: 'hernandez',
+  correo: 'ramon@gmail.com'
 }
 
   constructor() { }
