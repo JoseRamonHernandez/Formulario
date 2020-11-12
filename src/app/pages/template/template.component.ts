b06b278844fa8eb1e5e2d4f8c955e37b92e56e16
JoseRamonHernandez <67391682+JoseRamonHernandez@users.noreply.github.com / joseramonhernandezmunoz20@gmail.com>
2020-11-12 17:33:33 -0600

    video 194
    Servicio pais

--- src/app/pages/template/template.component.ts
+++ src/app/pages/template/template.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-template',
@@ -14,9 +15,15 @@ usuario = {
   correo: 'ramonhernandez@gmail.com'
 }
 
-  constructor() { }
+  constructor(private paisService: PaisService ) { }
 
   ngOnInit(): void {
+
+this.paisService.getPaises()
+    .subscribe(paises =>{
+      console.log(paises);
+    });
+
   }
 
   guardar(forma: NgForm)  {
